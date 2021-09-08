7c3ec215ccfbb72b03c6bf7c0af1d17aa487d335
jisoooit <77223718+jisoooit@users.noreply.github.com>
2021-09-08 17:13:14 +0900

    이진수

---
+++ dp/2193.py
@@ -0,0 +1,15 @@
+n=int(input())
+d=[[0 for i in range(2)] for j in range(91)]
+
+d[1][0]=0
+d[1][1]=1
+
+for i in range(2,n+1):
+  for j in range(2):
+    if j==0:
+      d[i][j]=d[i-1][0]+d[i-1][1]
+    elif j==1:
+      d[i][j]=d[i-1][0]
+  
+
+print(sum(d[n]))
